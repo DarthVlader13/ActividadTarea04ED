ad0fca366aee03fd8661a7fa06f2030e497f4300
Vlader <Vlader@DESKTOP-6HKH0SF>
2022-02-18 19:13:54 +0100

    JFHA: Subimos la primera versón de nuestra aplicación.

---
+++ ActividadTarea04ED/src/notas/Notas.java
@@ -0,0 +1,36 @@
+/**
+ * @version 1.0
+ */
+package notas;
+
+import java.util.Scanner;
+
+public class Notas {
+
+	/**
+	 * @author Vlader
+	 *
+	 */
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
+		int n = 0;
+		String c = "";
+		System.out.println("Introduzca una nota: ");
+		n = reader.nextInt();
+
+		if (n >= 0 && n < 5)
+			c = "Suspenso";
+		else if (n >= 5 && n < 7)
+			c = "Aprobado";
+		else if (n >= 7 && n < 9)
+			c = "Notable";
+		else if (n >= 9 && n <= 10)
+			c = "Sobresaliente";
+		else
+			c = "La nota introducida no es correcta";
+
+		System.out.println(c);
+		reader.close();
+
+	}
+}
