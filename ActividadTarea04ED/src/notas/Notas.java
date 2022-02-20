2ef5383ac0f9e70916154510bcc92d85c154e6b7
Vlader <Vlader@DESKTOP-6HKH0SF>
2022-02-20 02:32:10 +0100

    JFHA: Actividad 7
    7.	Documenta el código que has obtenido como resultado de todas las
    actividades anteriores de forma que quede claro lo que realiza: qué
    función realiza cada clase y/o método, cada uno de los parámetros
    necesarios, el autor, la versión, si se ha quedado algún método obsoleto
    y cualquier otra cuestión que consideres relevante. Una vez documentado
    el código genera el fichero javaDoc. Lo realizarás tanto en Eclipse como
    en Netbeans. Sube las modificaciones al repositorio de Github, siendo el
    comentario el enunciado de este ejercicio. Deberás presentar capturas de
    pantalla de todo lo que vas haciendo y de los apartados más relevantes
    del documento JavaDoc donde aparecen reflejados.

--- ActividadTarea04ED/src/notas/Notas.java
+++ ActividadTarea04ED/src/notas/Notas.java
@@ -1,21 +1,34 @@
 /**
+ * Esta clase define si una nota es suspensa o aprobada
  * @version 1.0
  */
 package notas;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author José Francisco Hernández Allen
+ *
+ */
 public class Notas {
 
 	/**
-	 * @author Vlader
+	 * @author José Francisco Hernández Allen
 	 *
+	 */
+
+	/**
+	 * 
+	 * @param args 
+	 * Esta es la clase main donde se pedirá que se introduzca una nota.
 	 */
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
-		int nota = 0;
+		int nota = 0; // Se declara la variable nota como entero y se inicia a 0.
 		String calificacion = "";
 		System.out.println("Introduzca una nota: ");
+		// Se imprime "Introduzca una nota" y pide leer el número a introducir.
 		nota = reader.nextInt();
 
 		notas.califica.califica(reader, nota);
@@ -23,20 +36,25 @@ public class Notas {
 	}
 
 	/**
-	 * @param reader
-	 * @param nota
-	 * @deprecated Use {@link notas.califica#califica(Scanner,int)} instead
+	 * @param reader lo usaremos para leer la nota
+	 * @param nota variable donde se guardará la nota introducida
+	 * @deprecated este metodo queda obseloteo por califica
+	 * Con el deprecated se está dejando claro que el método cali está en desuso
 	 */
 	public static void cali(Scanner reader, int nota) {
-		notas.califica.califica(reader, nota);
-	}
-
-	/**
-	 * @param reader
-	 * @param num_nota
-	 * @deprecated Use {@link califica#califica(Scanner,int)} instead
+		notas.califica.califica(reader, nota); }
+	
+	 /**
+	 * 
+	 * @param reader lo usaremos para leer la nota
+	 * @param num_nota variable donde se guardará la nota introducida
+	 * @deprecated este método queda obsoleto ya que lo hemos movido a la clase califica
+	 * Con el deprecated se está dejando claro que el método cali está en desuso
 	 */
 	public static void califica(Scanner reader, int num_nota) {
 		califica.califica(reader, num_nota);
+	 /**
+	  * Este método está depreciado también porque lo hemos movido a la clase califica que creamos.
+	  */
 	}
 }
--- ActividadTarea04ED/src/notas/califica.java
+++ ActividadTarea04ED/src/notas/califica.java
@@ -2,19 +2,29 @@ package notas;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author José Francisco Hernández Allen
+ *
+ */
 public class califica {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	/**
-	 * @param reader
-	 * @param num_nota
+	 * @param reader lo usaremos para leer la nota
+	 * @param num_nota variable donde se guardará la nota introducida
+	 * Esta clase ejecutará el método califica que nos dirá si la nota es aprobada,
+	 * suspensa, notable, sobresaliente o que no es correcta.
 	 */
 	public static void califica(Scanner reader, int num_nota) {
 		String calificacion;
+		/**
+		 * Este método ejecutará un else if si se cumplen los requisitos del número introducido.
+		 * Si es mayor o igual a 0 y menor que 5 será suspenso.
+		 * Si es mayor o igual a 5 y menor que 7 será aprobado.
+		 * Si es mayor o igual a 7 y menor que 9 será notable.
+		 * Si es mayor o igual a 9 y menor o igual que 10 será sobresaliente.
+		 * Si no se cumple ninguna de las anteriores el programa dirá que la nota no es correcta.
+		 */
 		if (num_nota >= 0 && num_nota < 5)
 			calificacion = "Suspenso";
 		else if (num_nota >= 5 && num_nota < 7)
@@ -25,6 +35,10 @@ public class califica {
 			calificacion = "Sobresaliente";
 		else
 			calificacion = "La nota introducida no es correcta";
+		
+		/**
+		 * Una vez hechas las comprobaciones del if else, se imprimirá el resultado.
+		 */
 	
 		System.out.println(calificacion);
 		reader.close();
